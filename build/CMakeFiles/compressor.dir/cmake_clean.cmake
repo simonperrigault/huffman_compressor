ad2a9304630548e62ebed4d5c0061c0890bc3c28
Simon Perrigault <simonperrigault22@gmail.com>
2025-01-23 20:38:18 +0100

    initial commit

---
+++ build/CMakeFiles/compressor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compressor.dir/main.cpp.o"
+  "CMakeFiles/compressor.dir/main.cpp.o.d"
+  "compressor"
+  "compressor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compressor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/compressor.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/compressor.dir/main.cpp.o: \
+ /home/simonp/entrainement/compression/main.cpp \
+ /usr/include/stdc-predef.h
