44b34ae4796332be6d8a427994985133c55af420
Simon Perrigault <simonperrigault22@gmail.com>
2025-01-27 00:21:29 +0100

    compress and decompress, just a little thing at the end

--- Compressor.cpp
+++ Compressor.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 #include <math.h>
 #include <bitset>
+#include <sstream>
 
 #include "Compressor.h"
 
@@ -66,10 +67,10 @@ void encode_tree(const Node* curr, std::string& output) {
 }
 
 void compress(const char* input_filename, const char* output_filename) {
-    std::fstream input_file(input_filename, std::ios::in | std::ios::binary);
-    std::fstream output_file(output_filename, std::ios::out | std::ios::binary);
+    std::ifstream input_file(input_filename, std::ios_base::binary);
+    std::ofstream output_file(output_filename, std::ios_base::binary);
 
-    uint8_t curr;
+    char curr;
     std::unordered_map<char, int> counter;
     while (input_file) {
         curr = input_file.get();
@@ -89,15 +90,94 @@ void compress(const char* input_filename, const char* output_filename) {
         heap.pop();
     }
 
-    inorder_node(heap.top().second, 1);
-
     std::array<std::string, 256> char_to_code;
     fill_char_to_code(heap.top().second, char_to_code, "");
 
+    for (int i = 0; i < 256; i++) {
+        std::cout << std::bitset<8>(i) << " : " << char_to_code[i] << "\n";
+    }
+
     std::string encoded_tree;
     encode_tree(heap.top().second, encoded_tree);
-    std::cout << encoded_tree << "\n";
+    uint32_t tree_size = encoded_tree.size();
+    for (int i = 0; i < 4; i++) {
+        output_file.put((tree_size >> (3-i)*8) & 0xFF);
+    }
+    output_file << encoded_tree;
+
+    uint8_t buffer = 0;
+    int i_buffer = 0;
+    input_file.clear();
+    input_file.seekg(0);
+
+    while (input_file) {
+        curr = input_file.get();
+        for (char c : char_to_code[(uint8_t)curr]) {
+            buffer <<= 1;
+            if (c == '1') {
+                buffer ^= 1;
+            }
+            i_buffer++;
+            if (i_buffer == 8) {
+                output_file.put(buffer);
+                buffer = 0;
+                i_buffer = 0;
+            }
+        }
+    }
+    output_file.put(buffer << (8-i_buffer)); // we put the last byte with some useless bits
+}
+
+void decode_tree(std::string&& curr, std::stringstream& ss, std::unordered_map<std::string, char>& code_to_char) {
+    if (!ss) return;
+
+    char c = ss.get();
+    if (c == '1') {
+        c = ss.get();
+        code_to_char[curr] = c;
+        return;
+    }
+    else if (c == '0') {
+        decode_tree(curr+'0', ss, code_to_char);
+        decode_tree(curr+'1', ss, code_to_char);
+    }
 }
 
 void decompress(const char* input_filename, const char* output_filename) {
+    std::ifstream input_file(input_filename, std::ios_base::binary);
+    std::ofstream output_file(output_filename, std::ios_base::binary);
+
+    uint32_t tree_size = 0;
+    char curr;
+    for (int i = 0; i < 4; i++) {
+        curr = input_file.get();
+        tree_size <<= 8;
+        tree_size ^= curr;
+    }
+
+    std::string tree(tree_size, '\0');
+    input_file.read(&tree[0], tree_size);
+    std::unordered_map<std::string, char> code_to_char;
+    std::stringstream ss(tree);
+    decode_tree("", ss, code_to_char);
+
+    for (auto kv : code_to_char) {
+        std::cout << std::bitset<8>(kv.second) << " : " << kv.first << "\n";
+    }
+    
+    std::string buffer;
+    while (input_file) {
+        curr = input_file.get();
+        for (int i = 7; i >= 0; i--) {
+            buffer += '0' + (curr >> i & 1);
+            if (code_to_char.count(buffer)) {
+                output_file << code_to_char[buffer];
+                if (code_to_char[buffer] == EOF) {
+                    input_file.setstate(std::ios_base::eofbit);
+                    break;
+                }
+                buffer.clear();
+            }
+        }
+    }
 }
